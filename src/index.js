928b6654c276abc681994aeef5a6cc1a364c9178
Nikhil Singh <nikhilsinghg18051999@gmail.com>
2025-02-25 05:20:29 +0530

    Setup the basic bodyparser to parse body data.

---
+++ src/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const bodyParser = require("body-parser");
+
+
+
+const { PORT } =  require('./config/serverConfig')
+
+
+
+
+const setupAndStartServer = async() => {
+    //create an express object.
+    const app = express()
+
+    app.use(bodyParser.json())
+    app.use(bodyParser.urlencoded({extended: true}));
+    const PORT = 3000;
+    app.listen(PORT,() => {
+        console.log(`Server Started at ${PORT}`);
+    }) 
+
+
+
+}
+
+setupAndStartServer();
+
+
