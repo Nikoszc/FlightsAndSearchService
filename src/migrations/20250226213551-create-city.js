4f2946b33f2cc751bfe6059184bd43f50b974713
Nikhil Singh <nikhilsinghg18051999@gmail.com>
2025-02-27 03:34:36 +0530

    Added city model and migration

---
+++ src/migrations/20250226213551-create-city.js
@@ -0,0 +1,33 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Cities', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Cities');
+  }
+};
+
+
+//Migrations are used to put constraints, properties and other features on the tables.\
+// We can make these changes/constraints/featuers in the models.js file as well but migrations help us with the tracking the changes n an incremental manner. We can revert to a previous state as desired.
